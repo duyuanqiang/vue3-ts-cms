961cbdd5f9c852c71fb29f578e5180089e4742b1
= <=>
2021-08-25 18:35:25 +0800

    init

---
+++ commit.bat
@@ -0,0 +1,8 @@
+echo "#vue3-ts-cms" >> README.md 
+git init 
+git add README.md 
+git commit -m "first commit" 
+git branch -M main 
+git remote add origin git@github.com:duyuanqiang/vue3-ts-cms.git
+git push -u origin main
+pause
